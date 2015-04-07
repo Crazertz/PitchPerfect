c7d7ce6d04d4e1fb9950b0f0272f2ea7291cace9
Preston Wertz <prestoncwertz@gmail.com>
2015-04-07 13:09:50 -0600

    record  add on

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -8,17 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func recordAudio(sender: UIButton)
+    {
+        println("in record audio")
+    }
+    
 
 
 }
