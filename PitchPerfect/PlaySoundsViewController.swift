c0a93a7ce82757f937f2cf99e2336de1d721ec1c
Preston Wertz <prestoncwertz@gmail.com>
2015-04-09 08:02:39 -0600

    add play sounds view

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import AVFoundation
 class PlaySoundsViewController: UIViewController
 {
 
@@ -18,11 +18,17 @@ class PlaySoundsViewController: UIViewController
     
     override func viewDidLoad()
     {
+        var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3")
         super.viewDidLoad()
+        var filePathUrl = NSURL.fileURLWithPath(filePath!)
+        var audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+        audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+        audioPlayer.enableRate = true
     }
 
     override func didReceiveMemoryWarning()
     {
+        
         super.didReceiveMemoryWarning()
     }
 }
